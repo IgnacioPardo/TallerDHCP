5edbfae9ca009b1feb8a16b00c5896ef8e347773
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2022-10-14 18:13:03 -0300

    feat(respuestas)

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# TallerDHCP
-Taller DHCP - TDIV UTDT
+# Taller DHCP - TDIV UTDT
+
+- ¿Qué tipos de paquetes `DHCP` capturaron? ¿Capturaron todos los tipos de paquetes del protocolo? Explicar cuáles paquetes se envían por Broadcast y cuáles no.
+
+Paquetes de tipo discover, request e inform. No capturamos paquetes de tipo Offer. Dejando de lado los paquetes inform, tanto request, offer y discover se envían por broadcast.
+
+- ¿Qué tipos de mensajes son parte del protocolo `DHCP`?
+
+Mensajes de tipo broadcast.
+
+- ¿Qué tipo de protocolo usa el mensaje `Ping`?
+
+Utiliza el protocolo `ARP`
+
+- ¿Qué protocolo de la capa de transporte fue utilizado?
+
+El protocolo `UDP`
+
+- ¿Cuál es el IP del servidor?
+
+La IP del servidor será donde esté corriendo el archivo `dhcp_server.py`
+
+- ¿Cuál es la máscara de subred de la red utilizada?
+
+Como estábamos conectados en una red clase $C$, la máscara subred es acorde, en este caso a la red $255.255.255.0$
--- udp_cliente.py
+++ udp_cliente.py
@@ -1,14 +1,21 @@
 import socket
+from sys import argv
 
-host = "" # IP del host
-port = 10000 # numero de puerto
-msg = "Hello, World!"
+if __name__ == '__main__':
 
-sock = socket.socket(socket.AF_INET, # Internet
-             socket.SOCK_DGRAM) # UDP
-             
-sock.sendto(msg.encode(), (host, port))
+    if len(argv) != 2 or argv[1][0] != '-' or len(argv[1]) > 4*4:
+        print('Modo de uso:\n python checkIP.py -{IP a chequear} -{mensaje}')
+        quit()
 
-data = sock.recv(1024).decode() # escuchamos la respuesta
-print('El paquete recibido fue: \n' + data)
-sock.close()
+    host = argv[1][1:] # IP del host
+    port = 10000 # numero de puerto
+    msg = argv[2][1:]
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                socket.SOCK_DGRAM) # UDP
+                
+    sock.sendto(msg.encode(), (host, port))
+
+    data = sock.recv(1024).decode() # escuchamos la respuesta
+    print('El paquete recibido fue: \n' + data)
+    sock.close()
